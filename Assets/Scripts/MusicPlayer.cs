12640f554cebf7a47da4e996e6cf78ef0beb3be1
sormmaster <34348746+sormmaster@users.noreply.github.com>
2020-07-16 12:38:43 -0500

    Ship can move now

---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicPlayer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("LoadLevel", 5f);
+
+    }
+
+    // Update is called once per frame
+    void LoadLevel()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+public class Player : MonoBehaviour
+{
+    [Tooltip("In m/s")][SerializeField] float xSpeed = 15f;
+    [Tooltip("In m")] [SerializeField] float xRange = 8f;
+    [Tooltip("In m/s")] [SerializeField] float ySpeed = 15f;
+    [Tooltip("In m")] [SerializeField] float yRange = 8f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateHorizontal();
+        UpdateVertical();
+    }
+
+    void UpdateHorizontal()
+    {
+        float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        float xOffSetOnFrame = xThrow * xSpeed * Time.deltaTime;
+        float rawXPos = transform.localPosition.x + xOffSetOnFrame;
+        float Clampedpos = Mathf.Clamp(rawXPos, -xRange, xRange);
+        transform.localPosition = new Vector3(Clampedpos, transform.localPosition.y, transform.localPosition.z);
+    }
+
+    void UpdateVertical()
+    {
+        float ythrow = CrossPlatformInputManager.GetAxis("Vertical");
+        float yoffSetOnFrame = ythrow * ySpeed * Time.deltaTime;
+        float rawYPos = transform.localPosition.y + yoffSetOnFrame;
+        float Clampedpos = Mathf.Clamp(rawYPos, -yRange, yRange);
+        transform.localPosition = new Vector3(transform.localPosition.x, Clampedpos, transform.localPosition.z);
+    }
+}
