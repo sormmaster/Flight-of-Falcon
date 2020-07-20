096c2f8aebe6dcae2376a36f81026c4ddab59b2f
sormmaster <34348746+sormmaster@users.noreply.github.com>
2020-07-19 22:19:47 -0500

    Fleshed out first level

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -9,6 +9,13 @@ public class Player : MonoBehaviour
     [Tooltip("In m")] [SerializeField] float xRange = 8f;
     [Tooltip("In m/s")] [SerializeField] float ySpeed = 15f;
     [Tooltip("In m")] [SerializeField] float yRange = 8f;
+    [SerializeField] float positionPitchFactor = -5f;
+    [SerializeField] float positionYawFactor = 2f;
+    [SerializeField] float controlRollFactor = -10f;
+    [SerializeField] float controlPitchFactor = -10f;
+    float xThrow;
+    float ythrow;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -20,11 +27,26 @@ public class Player : MonoBehaviour
     {
         UpdateHorizontal();
         UpdateVertical();
+        Rotation();
+    }
+
+    void Rotation()
+    {
+        float pitchByPosition = transform.localPosition.y * positionPitchFactor;
+        float pitchByThrow = ythrow * controlPitchFactor;
+        float pitch = pitchByPosition + pitchByThrow;
+
+        float rollByThrow = xThrow * controlRollFactor;
+        float yawByPosition = transform.localPosition.x * positionYawFactor;
+
+        float yaw = yawByPosition;
+        float roll = rollByThrow;
+        transform.localRotation = Quaternion.Euler(pitch, yaw, roll);
     }
 
     void UpdateHorizontal()
     {
-        float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
         float xOffSetOnFrame = xThrow * xSpeed * Time.deltaTime;
         float rawXPos = transform.localPosition.x + xOffSetOnFrame;
         float Clampedpos = Mathf.Clamp(rawXPos, -xRange, xRange);
@@ -33,7 +55,7 @@ public class Player : MonoBehaviour
 
     void UpdateVertical()
     {
-        float ythrow = CrossPlatformInputManager.GetAxis("Vertical");
+        ythrow = CrossPlatformInputManager.GetAxis("Vertical");
         float yoffSetOnFrame = ythrow * ySpeed * Time.deltaTime;
         float rawYPos = transform.localPosition.y + yoffSetOnFrame;
         float Clampedpos = Mathf.Clamp(rawYPos, -yRange, yRange);
