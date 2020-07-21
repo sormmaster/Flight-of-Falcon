cd54518573ab250e47a0b1c38d40d8bb763b24e9
sormmaster <34348746+sormmaster@users.noreply.github.com>
2020-07-21 12:31:08 -0500

    Updated Scripts and Prefabs

--- Assets/Scripts/LevelController.cs
+++ Assets/Scripts/LevelController.cs
@@ -1,14 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-
-public class MusicPlayer : MonoBehaviour
+public class LevelController : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
+        Invoke("LoadLevel", 5f);
 
+    }
+
+    // Update is called once per frame
+    void LoadLevel()
+    {
+        SceneManager.LoadScene(1);
     }
 
     void Awake()
